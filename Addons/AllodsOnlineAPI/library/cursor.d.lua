467c231800cec636f32b294f720b0178113ec552
termorey <nickfierydragon1995@mail.ru>
2024-08-07 13:17:53 +0300

    feat: added spacing

--- Addons/AllodsOnlineAPI/library/cursor.d.lua
+++ Addons/AllodsOnlineAPI/library/cursor.d.lua
@@ -5,6 +5,7 @@
 
 
 --[[ EVENTS --]]
+
 ---@alias EVENT_INGAME_UNDER_CURSOR_CHANGED "EVENT_INGAME_UNDER_CURSOR_CHANGED"
 
 ---@overload fun(eventFunction: fun(data: { state: string } | { state: "interface" } | { state: "main_view_3d_none" } | { state: "main_view_3d_unit", unitId: ObjectId } | { state: "main_view_3d_device", deviceId: ObjectId, canUse: boolean } | { state: "main_view_3d_astral_unit", unitId: ObjectId } | { state: "main_view_3d_interactive_device_element", deviceType: USDEV } | { state: "main_view_3d_interactive_device_element", deviceType: USDEV_SCANER, type: HT, name: WString | nil }), sysEventName: EVENT_INGAME_UNDER_CURSOR_CHANGED)
